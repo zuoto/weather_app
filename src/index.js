95fb653c35b45f7a4a05c8c9e20117495a8f1071
zuoto <72199908+zuoto@users.noreply.github.com>
2020-11-09 12:25:30 +0100

    changed the layout of the app

--- src/index.js
+++ src/index.js
@@ -1,41 +1,110 @@
-let now = new Date();
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let day = days[now.getDay()];
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
 
-let hour = now.getHours();
-let minutes = now.getMinutes();
-
-let times = document.querySelector(".time");
-times.innerHTML = `${day}, ${hour}:${minutes}`;
-
-function typeCity(event) {
-  event.preventDefault();
-  let input = document.querySelector("#cityTyped");
-  let city = document.querySelector("#city");
-  input = input.value;
-  city.innerHTML = input;
-  let apiKey = "59a15f9eb2d8a3d337f3a6cafe49e480";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${input}&units=metric&appid=${apiKey}`;
-  console.log(apiUrl);
-  axios.get(apiUrl).then(showWeather);
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${formatHours(timestamp)}`;
 }
 
-let form = document.querySelector("form");
-form.addEventListener("submit", typeCity);
+function formatHours(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${hours}:${minutes}`;
+}
+
+function search(city) {
+  let apiKey = "59a15f9eb2d8a3d337f3a6cafe49e480";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showWeather);
+  console.log(apiUrl);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#cityTyped");
+  search(cityInputElement.value);
+}
+
+function chooseIcon(response) {
+  let icon;
+  let weatherCondition = response.data.weather[0].main;
+
+  if (weatherCondition === "Clear") {
+    icon = "004-sun";
+  }
+
+  if (weatherCondition === "Rain" || weatherCondition === "Squall") {
+    icon = "008-heavy rain";
+  }
+
+  if (weatherCondition === "Snow") {
+    icon = "011-snow";
+  }
+
+  if (weatherCondition === "Thunderstorm") {
+    icon = "006-thunderstorm";
+  }
+
+  if (weatherCondition === "Clouds") {
+    icon = "001-cloudy";
+  }
+
+  if (
+    weatherCondition === "Fog" ||
+    weatherCondition === "Mist" ||
+    weatherCondition === "Smoke" ||
+    weatherCondition === "Haze" ||
+    weatherCondition === "Dust" ||
+    weatherCondition === "Ash" ||
+    weatherCondition === "Sand"
+  ) {
+    icon = "022-fog";
+  }
+
+  if (weatherCondition === "Tornado") {
+    icon = "028-tornado";
+  }
+
+  if (weatherCondition === "Drizzle") {
+    icon = "007-rain";
+  }
+
+  return icon;
+}
 
 function showWeather(response) {
+  let cityElement = document.querySelector("#city");
   let temp = document.querySelector("#temp");
   let precipitations = document.querySelector("#precipitation");
   let wind = document.querySelector("#wind");
   let humidity = document.querySelector("#humidity");
+
+  let image = document.querySelector("#pic");
+
+  let weatherCondition = response.data.weather[0].main;
+  image.innerHTML = ` <img src="imgs2/${chooseIcon(
+    response
+  )}.svg" id="bigWeatherImage">`;
+  let dateElement = document.querySelector("#time");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+
+  cityElement.innerHTML = response.data.name;
 
   let temperature = Math.round(response.data.main.temp);
   temp.innerHTML = `${temperature}°C`;
@@ -45,10 +114,10 @@ function showWeather(response) {
 
   let humidityDisp = Math.round(response.data.main.humidity);
   humidity.innerHTML = `Humidity: ${humidityDisp}%`;
-
-  let precipDisp = Math.round(response.data.clouds.all);
-  precipitations.innerHTML = `Precipitation: ${precipDisp}%`;
 }
+
+let form = document.querySelector("form");
+form.addEventListener("submit", handleSubmit);
 
 function currentButtonClicked(position) {
   let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
@@ -60,20 +129,21 @@ function currentButtonClicked(position) {
 }
 function currentWeather(response) {
   let temp = document.querySelector("#temp");
-  let precipitations = document.querySelector("#precipitation");
+  let dateElement = document.querySelector("#time");
+
   let wind = document.querySelector("#wind");
   let humidity = document.querySelector("#humidity");
   let temperature = Math.round(response.data.main.temp);
+
   temp.innerHTML = `${temperature}°C`;
 
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+
   let windDisp = Math.round(response.data.wind.speed);
-  wind.innerHTML = `Wind: ${windDisp}m/s`;
+  wind.innerHTML = `Wind: ${windDisp} m/s`;
 
   let humidityDisp = Math.round(response.data.main.humidity);
   humidity.innerHTML = `Humidity: ${humidityDisp}%`;
-
-  let precipDisp = Math.round(response.data.clouds.all);
-  precipitations.innerHTML = `Precipitation: ${precipDisp}%`;
 
   let city = document.querySelector("#city");
   city.innerHTML = response.data.name;
